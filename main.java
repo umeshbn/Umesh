a408d5eea6962191923bbae077b0484b047c23ab
umeshbn <umeshbneelaguri.xworkz@gmail.com>
2021-12-20 21:45:40 +0530

    umesh bhai

---
+++ main.java
@@ -0,0 +1,17 @@
+class Main {
+ public static void main(String[] args) {
+  
+   int[] age = new int[5];
+      age[0]=33;
+      age[2]=35;
+      age[1]=34;
+      age[3]=36;
+      age[4]=37;
+	
+   System.out.println("fisrt element " + age[0]);
+   System.out.println("Second Element: " + age[1]);
+   System.out.println("Third Element: " + age[2]);
+   System.out.println("Fourth Element: " + age[3]);
+   System.out.println(age [4]=333/age[4]);
+ }
+}
